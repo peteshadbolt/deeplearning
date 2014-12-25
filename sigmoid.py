883b5154408502b6085c8f389f850f9963668452
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-12-25 23:40:05 +0000

    Far more sensible approach

--- sigmoid.py
+++ sigmoid.py
@@ -1,30 +1,17 @@
 import numpy as np
 
-class neuron:
-    """ A sigmoid neuron """
-    def __init__(self, inputs):
-        self.inputs = inputs
-        self.weights = np.random.uniform(0, 1, len(inputs))
-        self.bias = np.random.rand()
+class NeuralNet:
+    """ A neural net of sigmoid neurons """
+    def __init__(self, widths):
+        self.widths = widths
+        self.nlayers = len(widths)
+        shapes = zip(widths[:-1], widths[1:])
+        self.weights = [np.random.randn(y, x) for x,y in shapes]
+        self.biases = [np.random.randn(y,1) for y in widths[1:]]
 
-    def get_output(self):
-        input_values=[neuron.get_output() for neuron in self.inputs]
-        dotproduct = sum(w*x for w,x in zip(self.weights, input_values))
-        z = dotproduct + self.bias
-        return 1/(1+np.exp(-z))
+    def __str__(self): return "Neural Net %s" % self.widths
 
-class source:
-    """ A source - the first layer of the net """
-    def __init__(self, value):
-        self.value=value
+n=NeuralNet([28*28, 15, 10])
+print n
 
-    def get_output(self):
-        return self.value
-
-sources = [source(0) for i in range(10)]
-first_layer = [neuron(sources) for i in range(10)]
-second_layer = [neuron(first_layer) for i in range(10)]
-output_node = neuron(second_layer)
-
-print output_node.get_output()
 
